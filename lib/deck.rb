634d963e1d4e38a32bb912a13f78cbdb4b82d3eb
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-11-18 14:43:27 -0700

    Add Iteration 2 functionality to Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,15 @@
 class Deck
+  attr_reader :cards
   def initialize(cards)
     @cards = cards
   end
+
+  def count
+    return @cards.count
+  end
+
+  def cards_in_category(category)
+    @cards.find_all {|card| card.category == category}
+  end
+
 end
